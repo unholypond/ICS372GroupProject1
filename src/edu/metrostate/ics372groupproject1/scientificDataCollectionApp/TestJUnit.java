ba5f5279c4f5eeb8bbfb3468817caec2f09f5ec5
Kekeli D Akouete <dosakouete@gmail.com>
2019-02-20 18:32:58 -0600

    Added TestJUnit class

---
+++ src/edu/metrostate/ics372groupproject1/scientificDataCollectionApp/TestJUnit.java
@@ -0,0 +1,14 @@
+package edu.metrostate.ics372groupproject1.scientificDataCollectionApp;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestJUnit {
+
+	@Test
+	public void test() {
+	
+	}
+
+}
